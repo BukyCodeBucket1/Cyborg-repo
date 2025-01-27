677e963fc09438d8dc64e00208ff014b2e27f9df
Ebuka Frank <ebukafrank@gmail.com>
2025-01-27 07:35:16 +0000

    basic setup

--- New_BDD/cypress.config.ts
+++ New_BDD/cypress.config.ts
@@ -1,9 +1,40 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  
+  watchForFileChanges:true,
+  requestTimeout:10000,
+  trashAssetsBeforeRuns:true,
+  chromeWebSecurity:false,modifyObstructiveCode:true,
+  screenshotOnRunFailure:true,
+  viewportHeight:1080,
+  viewportWidth:1920,
+  video:true,
+  //modifyObstructivecode:true,
+
+
+  env:{
+    "USERNAMEw":"",
+  },
+  
+  
+  
   e2e: {
+
+    baseUrl:"",
+    video:true,
+    experimentalModifyObstructiveThirdPartyCode:true,
+   
+    
+    specPattern:"cypress/e2e/features/**/*.{feature,features}",
+    
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
+      return require("./cypress/plugin.index.ts")(on, config);
+
+
     },
   },
 });
---
+++ New_BDD/cypress/plugin/index.ts
@@ -0,0 +1,15 @@
+const cucumber = require("cypress-cucumber-preprocessor");
+const browserify=require("@cypress/browserify-preprocessor");
+
+
+module.exports = (on: any, config: any)=>{
+
+    const options={
+        ...browserify.defaultOptions,
+        typescript:require.resolve("typescript"),
+    }
+
+    on("file:preprocessor", cucumber(options));
+
+
+}
