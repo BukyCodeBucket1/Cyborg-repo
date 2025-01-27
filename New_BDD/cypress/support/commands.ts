caa29c70e879e0a6532837e1c131eba5fbdbf224
Ebuka Frank <ebukafrank@gmail.com>
2025-01-27 02:41:39 +0000

    basic setup

---
+++ New_BDD/cypress/support/commands.ts
@@ -0,0 +1,59 @@
+/// <reference types="cypress" />
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+// declare global {
+//   namespace Cypress {
+//     interface Chainable {
+//       login(email: string, password: string): Chainable<void>
+//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+//     }
+//   }
+// }
+
+
+declare namespace Cypress {
+    interface Chainable{
+        login(username: string, password : string): Chainable<Element>;
+        launch(url:string): Chainable<Element>;
+        logout(): Chainable<Element>;
+
+    }
+
+ 
+}
+
+Cypress.Commands.add("launch", (url:string)=>{
+    cy.visit(url);
+});
+
+Cypress.Commands.add("login", (username:string, password:string)=>{
+    cy.get("username").type(username);
+    cy.get("password").type(password);
+    cy.get("loginButton").click;
+})
